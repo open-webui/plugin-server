b2bf9e29490f3582685212a200ddd5182fd5bb0f
Robin Bially <robin.bially@iits-consulting.de>
2024-06-26 17:33:10 +0200

    wip [skip ci]

--- apps/vector_store/main.py
+++ apps/vector_store/main.py
@@ -7,6 +7,7 @@ from fastapi import (
 from fastapi.middleware.cors import CORSMiddleware
 from openai.types.beta import VectorStore
 from openai.types.beta.vector_store import FileCounts
+from openai.types.beta.vector_store_deleted import VectorStoreDeleted
 from pydantic import BaseModel
 
 from utils.pipelines.auth import get_current_user
@@ -27,6 +28,11 @@ class CreateVectorStoreForm(BaseModel):
     metadata: dict | None
 
 
+class ModifyVectorStoreForm(BaseModel):
+    name: str | None
+    metadata: dict | None
+
+
 @app.post("/")
 async def create_vector_store(form_data: CreateVectorStoreForm,
                               user: str = Depends(get_current_user)):
@@ -41,6 +47,100 @@ async def create_vector_store(form_data: CreateVectorStoreForm,
             usage_bytes=123,
             metadata=form_data.metadata,
         )
+    except Exception as e:
+        print(e)
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"{str(e)}"
+        )
+
+
+@app.delete("/{vector_store_id}")
+async def delete_vector_store(vector_store_id: str,
+                              user: str = Depends(get_current_user)):
+    try:
+        return VectorStoreDeleted(
+            id=vector_store_id,
+            deleted=True,
+            object="vector_store.deleted",
+        )
+    except Exception as e:
+        print(e)
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"{str(e)}",
+        )
+
+
+@app.get("/")
+async def list_vector_stores(limit: int | None = None,
+                             order: str | None = None,
+                             after: str | None = None,
+                             before: str | None = None,
+                             user: str = Depends(get_current_user)):
+    try:
+        return {
+            "object": "list",
+            "data": [
+                VectorStore(
+                    id="store-123",
+                    created_at=561651,
+                    file_counts=FileCounts(cancelled=1, completed=1, failed=1, in_progress=1, total=1),
+                    name="asdf",
+                    object="vector_store",
+                    status="completed",
+                    usage_bytes=123,
+                    metadata={},
+                )
+            ]
+        }
+    except Exception as e:
+        print(e)
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"{str(e)}",
+        )
+
+
+@app.get("/{vector_store_id}")
+async def retrieve_vector_store(vector_store_id: str,
+                                user: str = Depends(get_current_user)):
+    try:
+        return VectorStore(
+            id=vector_store_id,
+            created_at=561651,
+            file_counts=FileCounts(cancelled=1, completed=1, failed=1, in_progress=1, total=1),
+            name="asdf",
+            object="vector_store",
+            status="completed",
+            usage_bytes=123,
+            metadata={},
+        )
+
+    except Exception as e:
+        print(e)
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"{str(e)}",
+        )
+
+
+@app.post("/{vector_store_id}")
+async def modify_vector_store(vector_store_id: str,
+                              form_data: ModifyVectorStoreForm,
+                              user: str = Depends(get_current_user)):
+    try:
+        return VectorStore(
+            id=vector_store_id,
+            created_at=561651,
+            file_counts=FileCounts(cancelled=1, completed=1, failed=1, in_progress=1, total=1),
+            name=form_data.name,
+            object="vector_store",
+            status="completed",
+            usage_bytes=123,
+            metadata=form_data.metadata,
+        )
+
     except Exception as e:
         print(e)
         raise HTTPException(
